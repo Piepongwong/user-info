0ab6832bede610110aa1db3f036ac21fff7c6e6b
Piepongwong <j.tonneyck@gmail.com>
2017-02-15 00:06:11 +0100

    working

--- postserver.js
+++ postserver.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
-var path    = require("path");
 var reader = require("./json-file-reader");
 var fs = require("fs");
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+	extended: true
 })); 
 
 app.set('view engine', 'pug');
@@ -15,6 +14,10 @@ app.set('views','./views');
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.get('/searchuser', function(req, res) {
+	res.render("search");
+})
+
+app.get('/', function(req, res) {
 	res.render("search");
 })
 
@@ -28,7 +31,7 @@ app.get('/showusers', function(req, res) {
 			res.send("No users! Create some or get the hell out of here!");
 		}
 		else {
-			res.render("show", {objectData});
+			res.render("show", objectData);
 		}
 	})
 })
@@ -36,31 +39,43 @@ app.get('/showusers', function(req, res) {
 app.post('/searchuserhandler', function(req, res) {
     var name = req.body.firstName;
 	reader("./user.json", function(objectData) {
-		  for(var i=0;i < objectData.length; i++) {
-	    	
+		for(var i=0;i < objectData.length; i++) {
 	    	if(objectData[i]["firstName"] === name) {
-	    		objectData = [objectData[i]];
-	    		res.render("showsearch", {objectData});
-	    	};
+	    		objectData = objectData[i];
+	    		res.render("showsearch", objectData);
+	    	}
 	    }
 	 })
-});
+});	
 
-app.post('/createuserhandler', function(req, res) {
-
+app.post('/createuserhandler', function(req, res, next) {
 	reader("./user.json", function(objectData) {
-		 
-			objectData.push(req.body);
-
-			json = JSON.stringify(objectData);
-
-			fs.writeFile("user.json", json);
-
-			res.end("User created");
-
-	    })
+		objectData.push(req.body);
+		json = JSON.stringify(objectData);
+		fs.writeFile("user.json", json);	
+		res.end("User Created");
+	})
+	next();
 })
 
+app.post('/createuserhandler', function(req, res) {
+	res.redirect("/");
+})
+
+app.post('/livesearch', function(req, res) {
+	var typed = req.body.typed;
+	var suggestion = "";
+	reader("./user.json", function(objectData) {
+		for(var i=0;i < (objectData.length); i++) {
+	    	var partialName = objectData[i]["firstName"].slice(0, typed.length);
+	    	if( partialName === typed) {	    		
+	    		suggestion = objectData[i]["firstName"];
+	    		break;
+	    	}
+	    }
+		res.send(suggestion);
+	})
+})	
 
 var server = app.listen(3000, function() {
 	var host = server.address().address;
